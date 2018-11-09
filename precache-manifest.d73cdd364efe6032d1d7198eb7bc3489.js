346c179c421b7b0889f560de8976ef057ea125cb
Daniel Krachkouski <danilyanich@gmail.com>
2018-11-09 05:01:39 +0300

    Updates

---
+++ precache-manifest.d73cdd364efe6032d1d7198eb7bc3489.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9777c546b7bb973e34d1",
+    "url": "/happy-birthday/static/css/main.69bd3fd3.chunk.css"
+  },
+  {
+    "revision": "9777c546b7bb973e34d1",
+    "url": "/happy-birthday/static/js/main.9777c546.chunk.js"
+  },
+  {
+    "revision": "fdd578806175e02a493e",
+    "url": "/happy-birthday/static/js/1.fdd57880.chunk.js"
+  },
+  {
+    "revision": "0f32a0ed5234233ef1a3",
+    "url": "/happy-birthday/static/js/runtime~main.0f32a0ed.js"
+  },
+  {
+    "revision": "9b61a0963641b2cbec3a229815e3bb65",
+    "url": "/happy-birthday/index.html"
+  }
+];
